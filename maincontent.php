99c4486fd287156c5e36985add776833b5da9e68
Chronos-png <ychronos@gmail.com>
2023-11-28 12:12:58 +0700

    footer

--- maincontent.php
+++ maincontent.php
@@ -1,16 +1,19 @@
-<div class="content">
-    Pariwisata merupakan salah satu sub sektor yang dapat meningkatkan perekonomian negara dan daerah, industri jasa ini sangat
-    berpengaruh pada perekonomian lokal juga dan merupakan pendorong perekonomian dunia yang dapat memutar uang skala dunia.
+<div class="maincontent">
+    <h2>Rekomendasi Wisata Agro Pamekasan Madura</h2>
+    <div class="paragraph">
+        Pariwisata merupakan salah satu sub sektor yang dapat meningkatkan perekonomian negara dan daerah, industri jasa ini sangat
+        berpengaruh pada perekonomian lokal juga dan merupakan pendorong perekonomian dunia yang dapat memutar uang skala dunia.
 
-    Meningkatkannya perkembangan teknologi informasi telah mewajibkan kepada industri pariwisata untuk maju dan menyesuaikan / adaptasi
-    pada teknologi, sehingga menjadikan peluang promosi daerah pariwisata lebih optimal.
+        Meningkatkannya perkembangan teknologi informasi telah mewajibkan kepada industri pariwisata untuk maju dan menyesuaikan / adaptasi
+        pada teknologi, sehingga menjadikan peluang promosi daerah pariwisata lebih optimal.
 
-    Secara umum teknologi informasi akan sangat bermanfaat dalam penyajian informasi yang cepat, mudah dan akurat yang sangat dibutuhkan oleh 
-    wisatawan. Salah satu dari sekian banyak teknologi informasi yang bermanfaat bagi wisatawan dan dapat diakses dengan mudah dari manapun 
-    adalah internet. (Palit, Talumingan and Rumagit, 2017).
+        Secara umum teknologi informasi akan sangat bermanfaat dalam penyajian informasi yang cepat, mudah dan akurat yang sangat dibutuhkan oleh 
+        wisatawan. Salah satu dari sekian banyak teknologi informasi yang bermanfaat bagi wisatawan dan dapat diakses dengan mudah dari manapun 
+        adalah internet. (Palit, Talumingan and Rumagit, 2017).
 
-    Halaman web ini dibuat berdasarkan Sistem Pendukung Keputusan Rekomendasi Wisata Agro Menggunakan Metode AHP, yang bertujuan untuk
-    membantu para pecinta alam dengan pemilihan rekomendasi wisata agro yang ada di madura, dimana AHP merupakan suatu model pendukung 
-    keputusan yang dikembangkan oleh Thomas L. Model pendukung keputusan ini akan menguraikan masalah multi faktor atau multi kriteria yang 
-    kompleks menjadi suatu hirarki, menurut Saaty (1993)
+        Halaman web ini dibuat berdasarkan Sistem Pendukung Keputusan Rekomendasi Wisata Agro Menggunakan Metode AHP, yang bertujuan untuk
+        membantu para pecinta alam dengan pemilihan rekomendasi wisata agro yang ada di madura, dimana AHP merupakan suatu model pendukung 
+        keputusan yang dikembangkan oleh Thomas L. Model pendukung keputusan ini akan menguraikan masalah multi faktor atau multi kriteria yang 
+        kompleks menjadi suatu hirarki, menurut Saaty (1993)
+    </div>
 </div>
