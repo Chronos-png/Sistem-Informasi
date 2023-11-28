362aa1ce60a32fde04f7b55820b9e5e51b293536
Chronos-png <ychronos@gmail.com>
2023-11-28 11:38:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include("header.php") ?>
+    <?php include("maincontent.php") ?>
+    <?php include("footer.php")?>
+</body>
+</html>
